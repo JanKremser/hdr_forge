756df85141bc406703015d08a968925fcf136a5e
j.kremser <github@nerdbrief.de>
2025-10-26 13:29:21 +0100

    grain analyzer

--- src/hdr_forge/analyze/grain_score.py
+++ src/hdr_forge/analyze/grain_score.py
@@ -1,8 +1,11 @@
+from contextlib import contextmanager
+import os
+os.environ["OPENCV_FFMPEG_LOGLEVEL"] = "quiet"
 import cv2
 import numpy as np
 from dataclasses import dataclass
 from typing import List, Tuple
-from hdr_forge.cli.cli_output import ProgressBarSpinner
+from hdr_forge.cli.cli_output import ProgressBarSpinner, print_err
 from hdr_forge.video import Video
 
 @dataclass
@@ -19,14 +22,17 @@ class GrainAnalyzer:
     def __init__(
         self,
         video: Video,
-        duration_sec: int | None = 5,
-        sample_rate: float | None = 1,
-        resize_width: int | None = 640
+        duration_sec: int | None = 20,
+        sample_rate: float | None = 2,
+        resize_width: int | None = 640,
+        start_sec: float | None = None
     ):
         self._video: Video = video
         self._duration_sec: int = duration_sec or 5
         self._sample_rate: float = sample_rate or 1
         self._resize_width: int = resize_width or 640
+        self._start_sec: float = start_sec or (video.get_duration_seconds() / 2 if start_sec == "middle" else 0)
+
         self._result: GrainResult = GrainResult()
 
     def _analyze_frame(self, frame: np.ndarray) -> float:
@@ -54,22 +60,44 @@ class GrainAnalyzer:
         if not cap.isOpened():
             raise RuntimeError("Video cannot be opened")
 
+        #fps = cap.get(cv2.CAP_PROP_FPS)
+        #total_frames = int(min(self._duration_sec * fps, cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+
         fps = self._video.get_fps()
-        total_frames = int(min(self._duration_sec * fps, self._video.get_total_frames()))
+        start_frame = int(self._start_sec * fps)
+        end_frame = min(
+            start_frame + int(self._duration_sec * fps),
+            self._video.get_total_frames()
+        )
         sample_interval = max(int(fps * self._sample_rate), 1)
+
 
         scores: List[float] = []
         spinner = ProgressBarSpinner("Analyzing grain...")
         spinner.start()
 
-        for i in range(0, total_frames, sample_interval):
-            cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+        for i in range(start_frame, end_frame, sample_interval):
+            safe_pos = max(i - int(fps * 1), 0)
+            cap.set(cv2.CAP_PROP_POS_FRAMES, safe_pos)
             ret, frame = cap.read()
-            if not ret:
-                break
 
-            score = self._analyze_frame(frame)
-            scores.append(score)
+            # Lies ein paar Frames, um Decoder zu stabilisieren
+            for _ in range(3):
+                spinner.update()
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+            if not ret:
+                continue
+
+            try:
+                score = self._analyze_frame(frame)
+                scores.append(score)
+            except Exception:
+                print_err("Error analyzing frame for grain score")
+                pass
+
             spinner.update()
 
         cap.release()
@@ -82,7 +110,7 @@ class GrainAnalyzer:
                 score=avg_score,
                 scores=scores
             )
-            spinner.stop(f"Detected grain category: {category}")
+            spinner.stop(f"Detected grain category: {category}, score: {avg_score:.4f}")
         else:
             spinner.stop()
 
