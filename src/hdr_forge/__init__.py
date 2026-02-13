5c269d5c8849099ef0ea0b5465ae42ae8503ebb7
j.kremser <github@nerdbrief.de>
2026-02-13 15:57:57 +0100

    bump up version

--- src/hdr_forge/__init__.py
+++ src/hdr_forge/__init__.py
@@ -4,6 +4,6 @@ A Python CLI tool for converting HDR10, Dolby Vision, and SDR video content
 to H.265/HEVC format with intelligent quality optimization.
 """
 
-__version__ = "0.8.3"
+__version__ = "1.0.0"
 __author__ = "Jan Kremser"
 __email__ = "git@nerdbrief.de"
